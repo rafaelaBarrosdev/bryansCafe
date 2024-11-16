0049081b35ae34c82723ecb921f49d24b9d9b821
rafaelaBarrosdev <rafaelaaabarros@gmail.com>
2024-11-16 13:09:17 +1100

    media query and other fixes

--- contact.js
+++ contact.js
@@ -40,3 +40,8 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .catch(error => console.error('Error fetching branches XML:', error));
 });
+
+function toggleMenu() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('expanded');
+}
